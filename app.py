57629eb1057c556936c5280c5017690f1a330646
AkramHussainChoudhury <akramchoudhury90@gmail.com>
2025-07-07 07:36:42 +0800

    Pushing code changes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask,request,render_template
+from flask import Flask, Response,request,render_template
 import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from src.pipeline.train_pipeline import TrainPipeline
 
 application=Flask(__name__)
 
@@ -14,6 +15,13 @@ app=application
 @app.route('/')
 def index():
     return render_template('index.html') 
+
+@app.route('/train',methods=['GET'])
+def train_model():
+    train_pipeline=TrainPipeline()
+    train_pipeline.train()
+
+    return Response("Training is successfull")
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
@@ -26,8 +34,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            reading_score=70,
+            writing_score=80
 
         )
         pred_df=data.get_data_as_data_frame()
